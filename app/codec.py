ec823a2631f78bae1430fb44fac6b9172511adf7
Michael Tarassov <michael@tarassov.me>
2021-02-17 20:52:27 +0300

    Add base62 encode/decode codec

---
+++ app/codec.py
@@ -0,0 +1,28 @@
+"""Base62 codec for generating short URL codes."""
+
+ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+BASE = len(ALPHABET)
+CODE_LENGTH = 6
+
+
+def encode(n: int) -> str:
+    """Encode a non-negative integer to a base62 string."""
+    if n < 0:
+        raise ValueError("Cannot encode negative integers")
+    if n == 0:
+        return ALPHABET[0]
+    digits = []
+    while n:
+        digits.append(ALPHABET[n % BASE])
+        n //= BASE
+    return "".join(reversed(digits))
+
+
+def decode(s: str) -> int:
+    """Decode a base62 string to a non-negative integer."""
+    result = 0
+    for char in s:
+        if char not in ALPHABET:
+            raise ValueError(f"Invalid base62 character: {char!r}")
+        result = result * BASE + ALPHABET.index(char)
+    return result
