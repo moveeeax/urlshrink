7a471e9806794881a3f7462a6ba5cbc101b316d8
Michael Tarassov <michael@tarassov.me>
2021-04-16 18:59:55 +0300

    Add SQLAlchemy URLEntry model

---
+++ app/models.py
@@ -0,0 +1,18 @@
+"""SQLAlchemy ORM model for persisted URL records."""
+
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class URLEntry(Base):
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    code = Column(String(16), unique=True, index=True, nullable=False)
+    url = Column(String(2048), nullable=False)
+    hits = Column(Integer, default=0, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
