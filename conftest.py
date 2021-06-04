d590fef8b19fb649d5559ffb9d2393f0ef46e34a
Michael Tarassov <michael@tarassov.me>
2021-06-04 23:22:00 +0300

    Add conftest.py for pytest root discovery

---
+++ conftest.py
@@ -0,0 +1,3 @@
+# conftest.py — pytest configuration at project root
+# No additional fixtures needed here; each test module imports from app.*
+# This file ensures pytest discovers tests/app as packages correctly.
